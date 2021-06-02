b9fb32b791405818b982fa565fb6c9fadbc64858
aikamatsui <aikamm777@gmail.com>
2021-06-02 19:37:42 +0900

    アラートできた

--- original/AddViewController.swift
+++ original/AddViewController.swift
@@ -11,7 +11,7 @@ import RealmSwift
 class AddViewController: UIViewController, UITextFieldDelegate{
     
     let realm = try! Realm()
-
+    
     @IBOutlet var dateLabel: UILabel!
     @IBOutlet var dateTextField: UITextField!
     @IBOutlet var motionLabel: UILabel!
@@ -39,39 +39,39 @@ class AddViewController: UIViewController, UITextFieldDelegate{
         dateTextField.keyboardType = UIKeyboardType.emailAddress
         
         let tapGR: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
-                tapGR.cancelsTouchesInView = false
-                self.view.addGestureRecognizer(tapGR)
+        tapGR.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tapGR)
         
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-                NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
         
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-    textField.resignFirstResponder()
+        textField.resignFirstResponder()
     }
-
+    
     @objc func dismissKeyboard() {
         self.view.endEditing(true)
     }
     
     @objc func keyboardWillShow(notification: NSNotification) {
-            if !contentsTextField.isFirstResponder {
-                return
-            }
+        if !contentsTextField.isFirstResponder {
+            return
+        }
         
-            if self.view.frame.origin.y == 0 {
-                if let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
-                    self.view.frame.origin.y -= keyboardRect.height
-                }
+        if self.view.frame.origin.y == 0 {
+            if let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+                self.view.frame.origin.y -= keyboardRect.height
             }
         }
+    }
     
     @objc func keyboardWillHide(notification: NSNotification) {
-            if self.view.frame.origin.y != 0 {
-                self.view.frame.origin.y = 0
-            }
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
         }
+    }
     
     @IBAction func happybuttonPush(_ sender: Any) {
         
@@ -80,7 +80,7 @@ class AddViewController: UIViewController, UITextFieldDelegate{
         goodbutton.setImage(UIImage(named: "green2"), for: .normal)
         sadbutton.setImage(UIImage(named: "blue2"), for: .normal)
         feelingNumber = 0
-        }
+    }
     
     @IBAction func smilebuttonPush(_ sender: Any) {
         
@@ -89,7 +89,7 @@ class AddViewController: UIViewController, UITextFieldDelegate{
         goodbutton.setImage(UIImage(named: "green2"), for: .normal)
         sadbutton.setImage(UIImage(named: "blue2"), for: .normal)
         feelingNumber = 1
-        }
+    }
     
     @IBAction func goodbuttonPush(_ sender: Any) {
         
@@ -98,7 +98,7 @@ class AddViewController: UIViewController, UITextFieldDelegate{
         goodbutton.setImage(UIImage(named: "green"), for: .normal)
         sadbutton.setImage(UIImage(named: "blue2"), for: .normal)
         feelingNumber = 2
-        }
+    }
     
     @IBAction func sadbuttonPush(_ sender: Any) {
         
@@ -107,23 +107,38 @@ class AddViewController: UIViewController, UITextFieldDelegate{
         goodbutton.setImage(UIImage(named: "green2"), for: .normal)
         sadbutton.setImage(UIImage(named: "blue"), for: .normal)
         feelingNumber = 3
-        }
+    }
     
     
     @IBAction func savediary() {
-
-                let diary = Diary()
+        
+        let diary = Diary()
         diary.date = self.dateTextField.text!
         diary.contents = self.contentsTextField.text!
         diary.feelingNumber = self.feelingNumber
-
+        
         let realm = try! Realm()
-        print(Realm.Configuration.defaultConfiguration.fileURL!)
-                try! realm.write {
-                    realm.add(diary.self)
-                }
-
-                self.dismiss(animated: true, completion: nil)
+        print(Realm.Configuration.defaultConfiguration.fileURL!)//Realmのファイルurlを取得
+        try! realm.write {
+            realm.add(diary.self)//Realmに保存する
+        }
+        
+        let alert: UIAlertController = UIAlertController(title: "保存完了",message: "日記を保存しました！",preferredStyle: .alert)
+        
+        alert.addAction(
+        UIAlertAction(
+        title: "OK",
+        style: .default,
+        handler: { action in//ボタンが押された時の動作
+        self.dismiss(animated: true, completion: nil)
+        print("OKボタンが押されました！")
+        
+        }))
+        
+        present(alert, animated: true, completion: nil)
         
     }
+    
+
 }
+
